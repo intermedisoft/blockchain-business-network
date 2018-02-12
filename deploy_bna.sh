a2283e3e3018f2f79e904e4e403e21670b405515
Seksit Disaro <zekzit@imed-mm.local>
2018-02-12 17:57:40 +0700

    fix type error

--- deploy_bna.sh
+++ deploy_bna.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 composer archive create -t dir -n .
-composer network deploy -a my-network@0.1.8.bna -A admin -S adminpw -c PeerAdmin@hlfv1
-composer card import -f admin@my-network.card
+composer network deploy -a depa-blockchain-network@1.0.0.bna -A admin -S adminpw -c PeerAdmin@hlfv1
+composer card import -f admin@depa-blockchain-network.card
