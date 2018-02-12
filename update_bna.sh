dcacbebebc36d5112c7a6260ec13f926807cb8c1
Seksit Disaro <zekzit@imed-mm.local>
2018-02-12 18:31:07 +0700

    fix typo error

--- update_bna.sh
+++ update_bna.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 composer archive create -t dir -n .
-composer network update -a depa-blockchain-networ@1.0.0.bna -c admin@depa-blockchain-network
+composer network update -a depa-blockchain-network@1.0.0.bna -c admin@depa-blockchain-network
